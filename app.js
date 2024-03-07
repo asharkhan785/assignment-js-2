6382acc4f1362e4e5fa8011bee8572376a4f6e79
asharkhan785 <154527325+asharkhan785@users.noreply.github.com>
2024-03-08 01:39:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// 1. Declare a variable called username.
+var username;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "Your Full Name"; // Replace "Your Full Name" with your actual full name.
+
+// 3. Write script to
+//    a) Declare a JS variable, titled message.
+//    b) Assign “Hello World” to variable message
+//    c) Display the message in an alert box.
+var message;
+message = "Hello World";
+alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+var studentName = "John Doe";
+var age = 20;
+var grade = "A";
+var bioDataMessage = "Student Name: " + studentName + "\nAge: " + age + "\nGrade: " + grade;
+alert(bioDataMessage);
+
+// 5. Write a script to display the following alert using one JS variable:
+var alertMessage = "This is a multiline alert.\nIt has multiple lines.";
+alert(alertMessage);
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address.
+var email = "example@example.com";
+alert("My email address is: " + email);
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+//    Display the following message in an alert box:
+var book = "A smarter way to learn JavaScript";
+alert("I am currently reading the book: " + book);
+
+// 8. Write a script to display this in the browser through JS.
+document.write("This is displayed in the browser through JavaScript.");
+
+// 9. Store the following string in a variable and show it in an alert and in the browser through JS.
+var decorativeString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(decorativeString);
+document.write(decorativeString);
